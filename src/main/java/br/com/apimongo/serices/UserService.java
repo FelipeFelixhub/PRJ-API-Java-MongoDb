14af53f67be794ac8928c102822df086583194ae
Felipe Felix <felipe.dfelix@gmail.com>
2023-09-12 14:32:26 -0300

    Deletando usuario com DELETE

--- src/main/java/br/com/apimongo/serices/UserService.java
+++ src/main/java/br/com/apimongo/serices/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.apimongo.domain.User;
 import br.com.apimongo.dto.UserDTO;
@@ -30,8 +31,15 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(@PathVariable String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	
+	
 
 }
