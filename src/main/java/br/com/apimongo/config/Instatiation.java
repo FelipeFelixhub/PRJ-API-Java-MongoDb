bbd57c44d9ca2cdb5724d165e824a6f7dcb9dae7
Felipe Felix <felipe.dfelix@gmail.com>
2023-09-13 14:27:22 -0300

    Criando entity Post com User aninhado

--- src/main/java/br/com/apimongo/config/Instatiation.java
+++ src/main/java/br/com/apimongo/config/Instatiation.java
@@ -1,12 +1,16 @@
 package br.com.apimongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.apimongo.domain.Post;
 import br.com.apimongo.domain.User;
+import br.com.apimongo.repositoy.PostRepository;
 import br.com.apimongo.repositoy.UserRepository;
 
 @Configuration
@@ -15,17 +19,29 @@ public class Instatiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		postRepository.deleteAll();
 		userRepository.deleteAll();
 		
 		User felipe = new User(null, "Felipe Felix", "felipe@gmail.com");
 		User carol = new User(null, "Caroline Felix", "carol@gmail.com");
 		User henrique = new User(null, "Henrique Felix", "henrique@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("13/09/2023"), "Partiu viagem", "Vou viajar para Londres. Abraços!", felipe);
+		Post post2 = new Post(null, sdf.parse("12/09/2023"), "Felicidade", "Coisas boas aconteceram", carol);
+		Post post3 = new Post(null, sdf.parse("10/09/2023"), "Vaiii Corinthians", "Bi Campeão Mundial", henrique);
+		
 		userRepository.saveAll(Arrays.asList(felipe,carol,henrique));
 		
+		postRepository.saveAll(Arrays.asList(post1,post2, post3));
 	}
 
 }
